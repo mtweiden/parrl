137a80d14a8592b63aab1dce82c20b76c87834c7
Justin Kalloor <justin.kalloor@gmail.com>
2024-05-04 12:26:40 -0700

    Adding Torch Parallelsim, RPC does not scale past 16 workers

--- parrl/agents/gauss_dvn_agent.py
+++ parrl/agents/gauss_dvn_agent.py
@@ -1,8 +1,11 @@
 """Based on: https://arxiv.org/pdf/2403.03950.pdf"""
 from __future__ import annotations
+from typing import Any, Iterator, Mapping, Sequence
 
 from einops import rearrange
+from random import random, choice
 
+from copy import deepcopy
 from torch import arange
 from torch import argmax
 from torch import device
@@ -11,6 +14,14 @@ from torch import nn
 from torch import no_grad
 from torch import Tensor
 from torch.nn import Parameter
+from torch.distributed.rpc import RRef
+import torch.optim as optim
+import torch.distributed.autograd as dist_autograd
+from torch.futures import Future
+from torch.optim import AdamW
+from torch.distributed.optim import DistributedOptimizer
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.nn.utils.clip_grad import clip_grad_norm_
 
 from copy import deepcopy
 
@@ -19,7 +30,6 @@ from parrl.core.model import Model
 from parrl.networks.softmoe import SoftMoELayer
 from parrl.networks.noisy import NoisyLinear
 from parrl.utils.gauss import HLGaussLoss
-
 
 class GaussDVNAgent(Agent):
 
@@ -32,6 +42,10 @@ class GaussDVNAgent(Agent):
         v_min: float,
         v_max: float,
         num_bins: int,
+        gradient_clip:float,
+        learning_rate: float,
+        gpu_rank: int = -1,
+        target_update_period: int = 10,
         noisy_net: bool = False,
         num_experts: int = 0,
         expert_latent_dim: int = 0,
@@ -46,7 +60,8 @@ class GaussDVNAgent(Agent):
         self.v_min = v_min
         self.v_max = v_max
         self.noisy_net = noisy_net
-
+        self.gradient_clip = gradient_clip
+        self.target_update_period = target_update_period
         # Hyperparameters from "Stop Regressing" paper
         zeta = (v_max - v_min) / num_bins  # bin widths
         sigma_zeta = 0.75
@@ -57,7 +72,7 @@ class GaussDVNAgent(Agent):
             num_bins=num_bins,
             sigma=sigma,
         )
-
+        self.gpu_rank = gpu_rank
         class _Critic(nn.Module):
             def __init__(self, encoder: nn.Module) -> None:
                 super().__init__()
@@ -94,16 +109,46 @@ class GaussDVNAgent(Agent):
                     z = rearrange(z, 'b m d -> b (m d)')
                 logits = self.logit_head(z)
                 return logits
-
-        self.critic = _Critic(encoder)
+        if gpu_rank >= 0:
+            gpu_model = _Critic(encoder).to(gpu_rank)
+            self.critic = DDP(gpu_model, device_ids=[gpu_rank])
+        else:
+            # Run critic on CPUå
+            self.critic = _Critic(encoder)
         self.target = deepcopy(self.critic)
+        if gpu_rank >= 0:
+            self.optimizer = AdamW(self.critic.parameters(), lr=learning_rate)
+        else:
+            self.optimizer = None
     
     def device(self) -> device:
         return self.critic.logit_head[1].weight.device
+
+    def parameters(self, recurse: bool = True) -> Iterator[Parameter]:
+        return self.critic.parameters()
     
+    def load_state_dict(self, state_dict: Mapping[str, Any], strict: bool = True, assign: bool = False):
+        state_dict = {k.removeprefix("target.").removeprefix("critic."): v.cpu() for k, v in state_dict.items()}
+        return self.target.load_state_dict(state_dict, strict, assign)
+    
+    def critic_state_dict(self, to_cpu=True):
+        if to_cpu:
+            return {k.removeprefix("module."): v.cpu() for k, v in self.critic.state_dict().items()}
+        return self.critic.state_dict()
+
+    def load_critic_state_dict(self, state_dict: Mapping[str, Any]):
+        self.critic.load_state_dict(state_dict)
+
     def critic_parameters(self) -> list[Parameter]:
         critic_params = list(self.critic.parameters())
         return critic_params
+
+    def critic_parameter_rrefs(self) -> list[RRef]:
+        critic_params = self.critic_parameters()
+        rref_params = []
+        for param in critic_params:
+            rref_params.append(RRef(param))
+        return rref_params
 
     def from_logits(self, logits: Tensor) -> Tensor:
         """
@@ -171,6 +216,63 @@ class GaussDVNAgent(Agent):
         qa_logits = self.model_q_logits(self.critic, state)
         qa_values = self.from_logits(qa_logits).squeeze()
         return qa_values
+
+    def select_action(self, state: Tensor) -> tuple[Tensor, float]:
+        q_values = self.qa_values(state)
+        ac = self.q_to_action(q_values)
+        ac = ac.item()
+
+        if q_values.ndim == 1:
+            qval = q_values[ac].item()
+        else:
+            qval = q_values[0, ac].item()
+
+        return ac, qval
+
+    def _train_step(self, batch: Tensor, batch_num: int) -> float:
+        """
+        Train the agent using the gathered experience.
+
+        This method is called by the `learn` method and is responsible for
+        training the agent using the gathered experience.
+
+        Args:
+            batch (Tensor): A minibatch of experience from the ReplayBuffer.
+
+            batch_num (int): The step in the current episode.
+        
+        Returns:
+            critic_loss (Tensor): The loss of the critic.
+        """
+        batch = tuple(
+            t.to(self.gpu_rank).float() for t in batch
+        )  # type: ignore
+        s, weights, indices = batch
+        # Update the critic
+        self.optimizer.zero_grad()
+        q_logits, target_values = self(s)
+        full_critic_loss = self.critic_loss(q_logits, target_values, weights)
+        critic_loss = full_critic_loss.mean()
+        critic_loss.backward()
+        clip_grad_norm_(self.critic_parameters(), self.gradient_clip)
+        self.optimizer.step()
+
+        # Update the target network
+        if batch_num % self.target_update_period == 0:
+            self.target.load_state_dict(self.critic.state_dict())
+
+        return full_critic_loss.abs().detach().cpu().numpy(), critic_loss.item(), indices
+
+    def critic_loss(self, 
+                    q_logits: Tensor, 
+                    target_values: Tensor, 
+                    weights: Tensor | None = None) -> Tensor:
+        loss = self.hlgauss(q_logits, target_values)
+        if weights is not None:
+            assert weights.shape == loss.shape
+            loss = loss * weights
+        return loss
+
 
     def q_value(self, state: Tensor, action: Tensor) -> Tensor:
         """
